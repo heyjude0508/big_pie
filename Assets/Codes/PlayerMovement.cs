1754cbc1a0b45d59ee4ee1cb57cd0397251b32e2
heyjude0508 <huangzx1993@gmail.com>
2026-02-22 23:35:16 +0800

    20260222 how to make a video game 1-3

---
+++ Assets/Codes/PlayerMovement.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 2000f;
+
+    void FixedUpdate()
+    {
+        // add a forward force
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        // add a right force
+        if (Input.GetKey("d")) {
+
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
+
+        }
+
+        // add a left force
+        if (Input.GetKey("d"))
+        {
+
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
+
+        }
+    }
+}
